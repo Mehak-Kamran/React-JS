f3844f3289d36d3729e7cd2b14116a0c2d73333d
Mehak Kamran <mehakkamran52@gmail.com>
2024-05-25 17:06:58 +0500

    making home and nav components

---
+++ src/nav.js
@@ -0,0 +1,14 @@
+const Navbar= () => {
+    return (
+        <nav className="nav">
+            <h1>The News</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Create Blog</a>
+            </div>
+        </nav>
+        
+      );
+}
+ 
+export default Navbar;
