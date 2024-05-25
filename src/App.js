84e37dd3b7dc76577105d99fab7035757df94a5f
Mehak Kamran <mehakkamran52@gmail.com>
2024-05-25 16:42:45 +0500

    adding dynamic content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi</h1>
+function App(){
+  const title="Hi";
+  let link="https://www.google.com/";
+  return(
+    <div className='cont'>
+      <h1>{title}</h1> {/*value using var*/}
+      <h2>{ "mehak"}</h2>{/*direct val*/}
+      <a href={link}> Google</a>
     </div>
+    
   );
 }
+
 
 export default App;
