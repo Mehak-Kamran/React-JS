3f916dcb37e27d17eddd2c63c1876518a58e62e2
Mehak Kamran <mehakkamran52@gmail.com>
2024-05-26 13:13:35 +0500

    click events handling

--- src/home.js
+++ src/home.js
@@ -1,7 +1,15 @@
 function Home(){
+    const handleClick=(e)=>{
+        console.log("Hello",e.target)
+}
+const handleClickButton2=(name,e)=>{
+    console.log(name+" "+"You clicked me again",e);
+}
     return(
         <div className="home">
             <h1>Home</h1>
+            <button onClick={handleClick}>Click Me!</button>
+            <button onClick={(e)=>{handleClickButton2("mak",e)}}>Click Me too!</button>
         </div>
 
     )
