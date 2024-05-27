acc4ef7fc72621162753a206f28c8b5e82c88fd6
Mehak Kamran <mehakkamran52@gmail.com>
2024-05-27 22:47:15 +0500

    use state hooks

--- src/home.js
+++ src/home.js
@@ -1,18 +1,35 @@
-function Home(){
-    const handleClick=(e)=>{
-        console.log("Hello",e.target)
-}
-const handleClickButton2=(name,e)=>{
-    console.log(name+" "+"You clicked me again",e);
-}
-    return(
-        <div className="home">
-            <h1>Home</h1>
-            <button onClick={handleClick}>Click Me!</button>
-            <button onClick={(e)=>{handleClickButton2("mak",e)}}>Click Me too!</button>
-        </div>
+// function Home(){
+//     const handleClick=(e)=>{
+//         console.log("Hello",e.target)
+// }
+// const handleClickButton2=(name,e)=>{
+//     console.log(name+" "+"You clicked me again",e);
+// }
+//     return(
+//         <div className="home">
+//             <h1>Home</h1>
+//             <button onClick={handleClick}>Click Me!</button>
+//             <button onClick={(e)=>{handleClickButton2("mak",e)}}>Click Me too!</button>
+//         </div>
 
+//     )
+// }
+import {useState} from 'react'
+
+function Home(){
+    const [name,setName]=useState("hafsa adil")
+
+    const handle=()=>{
+      setName("hafsa sajid")
+    }
+    return(
+        <div>
+            <h1>Home Page</h1>
+            <button onClick={handle}>After Marriage</button>
+            <p>Hi her name is {name} </p>
+        </div>
     )
+
 }
 
 export default Home;
