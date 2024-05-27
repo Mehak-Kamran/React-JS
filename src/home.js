1b952b96cdd5f4e087a3f7a98ffcb5d5e5827ecf
Mehak Kamran <mehakkamran52@gmail.com>
2024-05-27 22:57:24 +0500

    use state eg2

--- src/home.js
+++ src/home.js
@@ -14,22 +14,36 @@
 
 //     )
 // }
+// import {useState} from 'react'
+
+// function Home(){
+//     const [name,setName]=useState("hafsa adil")
+
+//     const handle=()=>{
+//       setName("hafsa sajid")
+//     }
+//     return(
+//         <div>
+//             <h1>Home Page</h1>
+//             <button onClick={handle}>After Marriage</button>
+//             <p>Hi her name is {name} </p>
+//         </div>
+//     )
+
+// }
 import {useState} from 'react'
-
-function Home(){
-    const [name,setName]=useState("hafsa adil")
-
-    const handle=()=>{
-      setName("hafsa sajid")
+let Home=()=>{
+    let [color,setColor]=useState("Your fav color is pink")
+    let handleButton=()=>{
+        setColor("Your lucky color is Blue")
     }
     return(
         <div>
-            <h1>Home Page</h1>
-            <button onClick={handle}>After Marriage</button>
-            <p>Hi her name is {name} </p>
+            <h1>Home</h1>
+            <button onClick={handleButton}>Click to see your lucky color</button>
+            <p>{color}</p>
         </div>
+        
     )
-
 }
-
 export default Home;
